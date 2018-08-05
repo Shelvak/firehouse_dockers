68156adccd42175d1c641193041e3229b8d6c575
Firehouse server <firehouse@server.org>
2018-08-05 16:24:36 -0300

    Deleting

--- attach.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-echo $1
-container=$(docker ps|grep "firehousedockers_$1" | awk '{ print $1 }')
-echo $container
-
-docker exec -it $container /bin/bash
--- broadcast/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pulseaudio -D
-echo "Rake:"
-cd /firehouse_audio
-bundle exec rake
