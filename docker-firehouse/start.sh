7ecc576e8e76f52568fd7941c042dedc4a9aa751
Firehouse server <firehouse@server.org>
2018-08-11 21:16:27 -0300

    Start firehouse script

--- docker-firehouse/start.sh
+++ docker-firehouse/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Mover el codigo al volumen
 mkdir /firehouse/tmp
 
 # Bundler y rake ya estan incluidos en la imagen
 echo "#### Inicializando Rails App"
-cd /firehouse && bundle install --without development test --deployment --jobs=4 && bundle exec rake assets:precompile && bundle exec unicorn -c /firehouse/config/unicorn.rb -E production
+cd /firehouse 
+bundle install --without development test --deployment --jobs=4 
+bundle exec rake assets:precompile 
+bundle exec rake db:migrate
+bundle exec unicorn -c /firehouse/config/unicorn.rb -E production
