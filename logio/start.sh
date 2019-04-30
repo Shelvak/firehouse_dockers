07eee7fe7c7016e0f947c1b376f686c607bd737b
Néstor Coppi <nestorcoppi@gmail.com>
2019-04-29 21:50:59 -0300

    Cleanup && Docker-compose order

--- logio/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting server"
-log.io-server&
-sleep 1
-
-echo "starting harvester"
-
-log.io-harvester
--- redis/start.sh
+++
@@ -1,5 +0,0 @@
-#Fix for warning
-sysctl vm.overcommit_memory=1
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-
-redis-server /etc/redis.conf
